abf3b03355581fd757848be36a8a412c3d8f36a6
Cristian <crismonsalve4@gmail.com>
2023-12-10 19:32:30 -0500

    Before making some refactoring to the chessboard representation

---
+++ pieces.js
@@ -0,0 +1,255 @@
+// Dark pieces
+
+const dKing = {
+    player: 'black',
+    name: 'king',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_kdt45.svg' alt='' width = '100%'></div>"
+}
+// dKing.setAttribute('src', 'img/Chess_kdt45.svg');
+
+const dQueen = {
+    player: 'black',
+    name: 'queen',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_qdt45.svg' alt='' width = '100%'></div>"
+}
+// dQueen.setAttribute('src', 'img/Chess_qdt45.svg');
+
+const dRook1 = {
+    player: 'black',
+    name: 'rook',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_rdt45.svg' alt='' width = '100%'></div>"
+}
+
+const dRook2 = {
+    player: 'black',
+    name: 'rook',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_rdt45.svg' alt='' width = '100%'></div>"
+}
+
+
+// dRook.setAttribute('src', 'img/Chess_rdt45.svg');
+
+const dBishop1 = {
+    player: 'black',
+    name: 'bishop',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_bdt45.svg' alt='' width = '100%'></div>"
+}
+
+const dBishop2 = {
+    player: 'black',
+    name: 'bishop',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_bdt45.svg' alt='' width = '100%'></div>"
+}
+
+// dBishop.setAttribute("src", 'img/Chess_bdt45.svg');
+
+const dKnight1 = {
+    player: 'black',
+    name: 'knight',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_ndt45.svg' alt='' width = '100%'></div>"
+}
+
+const dKnight2 = {
+    player: 'black',
+    name: 'knight',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_ndt45.svg' alt='' width = '100%'></div>"
+}
+
+// dKnight.setAttribute("src", 'img/Chess_ndt45.svg');
+
+const dPawn1 = {
+    player: 'black',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_pdt45.svg' alt='' width = '100%'></div>"
+}
+
+const dPawn2 = {
+    player: 'black',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_pdt45.svg' alt='' width = '100%'></div>"
+}
+
+const dPawn3 = {
+    player: 'black',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_pdt45.svg' alt='' width = '100%'></div>"
+}
+
+const dPawn4 = {
+    player: 'black',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_pdt45.svg' alt='' width = '100%'></div>"
+}
+
+const dPawn5 = {
+    player: 'black',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_pdt45.svg' alt='' width = '100%'></div>"
+}
+
+const dPawn6 = {
+    player: 'black',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_pdt45.svg' alt='' width = '100%'></div>"
+}
+
+const dPawn7 = {
+    player: 'black',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_pdt45.svg' alt='' width = '100%'></div>"
+}
+
+const dPawn8 = {
+    player: 'black',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_pdt45.svg' alt='' width = '100%'></div>"
+}
+// dPawn.setAttribute("src", 'img/Chess_bdt45.svg');
+
+// Light pieces
+
+const lKing = {
+    player: 'white',
+    name: 'king',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_klt45.svg' alt='' width = '100%'></div>"
+}
+
+// lKing.setAttribute('src', 'img/Chess_klt45.svg');
+
+const lQueen = {
+    player: 'white',
+    name: 'queen',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_qlt45.svg' alt='' width = '100%'></div>"
+}
+
+// lQueen.setAttribute('src', 'img/Chess_qlt45.svg');
+
+const lRook1 = {
+    player: 'white',
+    name: 'rook',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_rlt45.svg' alt='' width = '100%'></div>"
+}
+
+const lRook2 = {
+    player: 'white',
+    name: 'rook',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_rlt45.svg' alt='' width = '100%'></div>"
+}
+
+// lRook.setAttribute('src', 'img/Chess_rlt45.svg');
+
+const lBishop1 = {
+    player: 'white',
+    name: 'bishop',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_blt45.svg' alt='' width = '100%'></div>"
+}
+
+const lBishop2 = {
+    player: 'white',
+    name: 'bishop',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_blt45.svg' alt='' width = '100%'></div>"
+}
+
+
+// lBishop.setAttribute("src", 'img/Chess_blt45.svg');
+
+const lKnight1 = {
+    player: 'white',
+    name: 'knight',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_nlt45.svg' alt='' width = '100%'></div>"
+}
+
+const lKnight2 = {
+    player: 'white',
+    name: 'knight',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_nlt45.svg' alt='' width = '100%'></div>"
+}
+
+// lKnight.setAttribute("src", 'img/Chess_nlt45.svg');
+
+const lPawn1 = {
+    player: 'white',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_plt45.svg' alt='' width = '100%'></div>"
+}
+
+const lPawn2 = {
+    player: 'white',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_plt45.svg' alt='' width = '100%'></div>"
+}
+
+const lPawn3 = {
+    player: 'white',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_plt45.svg' alt='' width = '100%'></div>"
+}
+
+const lPawn4 = {
+    player: 'white',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_plt45.svg' alt='' width = '100%'></div>"
+}
+
+const lPawn5 = {
+    player: 'white',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_plt45.svg' alt='' width = '100%'></div>"
+}
+
+const lPawn6 = {
+    player: 'white',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_plt45.svg' alt='' width = '100%'></div>"
+}
+
+const lPawn7 = {
+    player: 'white',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_plt45.svg' alt='' width = '100%'></div>"
+}
+
+const lPawn8 = {
+    player: 'white',
+    name: 'pawn',
+    hasItMoved: false,
+    pieceHtml: "<div class = 'piece'><img src='img/Chess_plt45.svg' alt='' width = '100%'></div>"
+}
+
+// lPawn.setAttribute("src", 'img/Chess_blt45.svg');
+
+
+const emptySquare = {
+    pieceHtml: ''
+}
